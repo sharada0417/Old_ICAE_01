b0cacf6aeb1efd40099e0ce91056683f0fbac553
sharada417 <sharada.kanchana2001@gmail.com>
2025-06-30 22:24:24 +0530

    join table with undergraduate_id and session_id

--- SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
+++ SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
@@ -1,7 +1,12 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity //table creation
 public class UnderGraduate {
@@ -13,4 +18,12 @@ public class UnderGraduate {
 	private Long phone;
 	private String degree;
 	private String university;
+	
+	@ManyToMany
+	@JoinTable(
+		name = "under_grarduate_sessions",
+		joinColumns = @JoinColumn(name= "undergraduate_id"),
+		inverseJoinColumns = @JoinColumn(name = "session_id")
+	)
+	private List<Session> sessions;
 }
