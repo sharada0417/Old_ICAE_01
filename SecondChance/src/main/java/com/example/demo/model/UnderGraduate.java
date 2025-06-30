16b9157b0e9615315f0dcfdfc8517511d6aa5d16
sharada417 <sharada.kanchana2001@gmail.com>
2025-06-30 21:33:45 +0530

    undergraduate model

--- SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
+++ SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
@@ -1,5 +1,11 @@
 package com.example.demo.model;
 
 public class UnderGraduate {
-
+	private String id;
+	private String name;
+	private String email;
+	private String gender;
+	private Long phone;
+	private String degree;
+	private String university;
 }
