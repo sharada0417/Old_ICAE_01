bdad894ed15295a9cb6d544c7ed2ae777374f005
sharada417 <sharada.kanchana2001@gmail.com>
2025-06-30 21:55:50 +0530

    sesion model join with worksop_id

--- SecondChance/src/main/java/com/example/demo/model/Session.java
+++ SecondChance/src/main/java/com/example/demo/model/Session.java
@@ -5,6 +5,8 @@ import java.time.LocalTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity //table creation
 public class Session {
@@ -14,4 +16,8 @@ public class Session {
 	private String description;
 	private LocalDate date;
 	private LocalTime time;
+	
+	@ManyToOne
+	@JoinColumn(name = "workshop_id")
+	private Workshop workshop;
 }
