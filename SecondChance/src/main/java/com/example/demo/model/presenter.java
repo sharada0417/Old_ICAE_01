864fbb76a270972f8380bfcb9649ee29bd62e3a7
sharada417 <sharada.kanchana2001@gmail.com>
2025-06-30 22:13:40 +0530

    join table (presenter_is , session_id)

--- SecondChance/src/main/java/com/example/demo/model/presenter.java
+++ SecondChance/src/main/java/com/example/demo/model/presenter.java
@@ -1,7 +1,12 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity //table creation
 public class presenter {
@@ -14,4 +19,13 @@ public class presenter {
 	private String affliation;
 	private String country;
 	private String jobTitle;
+	
+	@ManyToMany
+	@JoinTable(
+		name = "presenter_sessions",
+		joinColumns = @JoinColumn(name = "presenters_id"),
+		inverseJoinColumns = @JoinColumn(name = "session_id")
+	)
+	
+	private List<Session> sessions;
 }
