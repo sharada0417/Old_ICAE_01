356fcb73e78993cc38f32d4ac1232134eeb65f50
sharada417 <sharada.kanchana2001@gmail.com>
2025-06-30 21:27:15 +0530

    presenter model

--- SecondChance/src/main/java/com/example/demo/model/presenter.java
+++ SecondChance/src/main/java/com/example/demo/model/presenter.java
@@ -1,5 +1,12 @@
 package com.example.demo.model;
 
 public class presenter {
-
+	private String id;
+	private String name;
+	private String email;
+	private String gender;
+	private Long phone;
+	private String affliation;
+	private String country;
+	private String jobTitle;
 }
