7343ed2d07da940e376d4d21542983b8acaa0e57
sharada417 <sharada.kanchana2001@gmail.com>
2025-06-30 22:06:50 +0530

    presenter to sesion(MTM) , sessions to postgraduate (MTM) , sessions to undergraduate (MTM)

--- SecondChance/src/main/java/com/example/demo/model/Session.java
+++ SecondChance/src/main/java/com/example/demo/model/Session.java
@@ -2,10 +2,12 @@ package com.example.demo.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
 @Entity //table creation
@@ -18,6 +20,20 @@ public class Session {
 	private LocalTime time;
 	
 	@ManyToOne
-	@JoinColumn(name = "workshop_id")
-	private Workshop workshop;
+	@JoinColumn(name = "workshop_id")//foreign key
+	private Workshop workshop;//workshop object
+	
+	//relationship presenter(many) to sessions(many)
+	@ManyToMany(mappedBy = "sessions")
+	private List<presenter> presenters;
+	
+	//relationship sessions(many) to postgraduate(many)
+	@ManyToMany(mappedBy = "sessions")
+	private List<PostGraduate> postGraduates;
+	
+	//relationship session(many) to undergraduate
+	@ManyToMany(mappedBy = "sessions")
+	private List<UnderGraduate> underGraduates;
+	
+	
 }
