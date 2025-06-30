7bdb86de19943b440f8e48892933a8d8303e3d51
sharada417 <sharada.kanchana2001@gmail.com>
2025-06-30 21:20:31 +0530

    create postgraduate model

---
+++ SecondChance/src/main/java/com/example/demo/model/PostGraduate.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+public class PostGraduate {
+	private Long id;
+	private String name;
+	private String description;
+	private LocalDate startDate;
+	private LocalDate endDate;
+}
